36a47b1de520c3e95482c3c2eb523400387ce0ff
Luca Genova <48278825+Lucajett99@users.noreply.github.com>
2020-11-04 13:07:31 +0100

    Update all from GitLab

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Twitter_Tracker
- A simple web application to collect tweets from Twitter, analyze and display them on word clouds or maps.
+# Twitter Tracker
+
+A simple web application to collect tweets from Twitter, analyze and display them on word clouds or maps.
+
+Per farlo partire usare il comando nella directory generale: npm start <br>
+Dal terminale integrato in VS Code oppure da cmd <br>
+Aprite il browser in localhost://3000
--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './css/App.css';
-import logo_twitter from './img/twitter.jpg' // relative path to image 
+import logo_twitter from './img/twitter.jpg' // relative path to image
+import $ from 'jquery';
+
 
 function App() {
+  $.ajax({
+    url: "https://api.twitter.com/2/tweets",
+    data: {authorization : "Bearer $AAAAAAAAAAAAAAAAAAAAAMrVIwEAAAAAIzz71ZCU5cAtvPDMmFpOK0gWhDE%3DK1yPC7sVQGGs6xgYh0xnaWGKaFbljoaYmEqj6iaxVQyVRaxsQs"},
+    success: function(response){
+      console.log(response);
+    },
+  });
+
   return (
     <div className="App">
       <header className="App-header">
